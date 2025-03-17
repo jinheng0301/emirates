f96940bbec7e4fac3cd7d5bb5c3b072c9b12af37
jinheng0301 <95295108+jinheng0301@users.noreply.github.com>
2025-03-17 11:34:52 +0800

    navigate to the booking screen from trips screen.

--- emirates/lib/screens/booking_screen.dart
+++ emirates/lib/screens/booking_screen.dart
@@ -5,6 +5,17 @@ class BookingScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Booking screen',
+          style: TextStyle(
+            color: Colors.black,
+            fontSize: 20,
+            fontWeight: FontWeight.w300,
+          ),
+        ),
+      ),
+    );
   }
 }
--- emirates/lib/screens/trips_screen.dart
+++ emirates/lib/screens/trips_screen.dart
@@ -1,3 +1,5 @@
+import 'package:emirates/screens/booking_screen.dart';
+import 'package:emirates/screens/upcoming_trip_display.dart';
 import 'package:flutter/material.dart';
 
 class TripsScreen extends StatelessWidget {
@@ -5,6 +7,65 @@ class TripsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(child: UpcomingTripDisplay()),
+          Padding(
+            padding: EdgeInsets.all(16),
+            child: Container(
+              height: 50,
+              width: double.infinity,
+              alignment: Alignment.center,
+              decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: Text(
+                'Add booking',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w300,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 5),
+          GestureDetector(
+            onTap: () {
+              Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (BuildContext context) => BookingScreen(),
+                ),
+              );
+            },
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: Container(
+                height: 50,
+                width: double.infinity,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Text(
+                  'Book a flight',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
